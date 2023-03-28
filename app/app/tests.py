bba72080432aa7e13960b88a879219bbf24681ac
seunghyun Kim <seankim@mediqutious.com>
2023-03-28 10:18:38 +0900

    SimpleTestCase

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,10 @@
 from django.test import SimpleTestCase
 from app import calc
+
+
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res = calc.add(3,8)
+        res = calc.add(3, 8)
         return self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
