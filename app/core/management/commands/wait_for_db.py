f6252eca936f69e6214d349eae9a1dc2fb16fcaf
seunghyun Kim <seankim@mediqutious.com>
2023-03-31 12:03:35 +0900

    wait_for_db 테스트 커맨드 케이스 추가

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,19 @@
+import time
+
 from django.core.management.base import BaseCommand
+from psycopg2 import OperationalError as Psycopg2OpError
+from django.db.utils import OperationalError
+
 
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('Waiting for database...')
+        db_ready = False
+        while db_ready is False:
+            try:
+                self.check(databases=['default'])
+                db_ready = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available!'))
